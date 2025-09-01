07b5f53052eb093eae2a3e327f32b700e22b9066
EmsDz <Esmarlyndiaz23@gmail.com>
2025-09-01 11:26:13 -0400

    Create login screen

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,54 @@
+import { Button, ButtonText } from '@/components/ui/button';
+import { FormControl } from '@/components/ui/form-control';
+import { Heading } from '@/components/ui/heading';
+import { HStack } from '@/components/ui/hstack';
+import { Input, InputField, InputIcon, InputSlot } from '@/components/ui/input';
+import { Text } from '@/components/ui/text';
+import { VStack } from '@/components/ui/vstack';
+import { EyeIcon, EyeOffIcon } from 'lucide-react-native';
+import { useState } from 'react';
+
+export default function LoginScreen() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  return (
+    <FormControl
+      className="p-4 border rounded-lg max-w-[500px] border-outline-300 bg-white m-2"
+    >
+      <VStack space="xl">
+        <Heading className="text-typography-900 leading-3 pt-3">Login</Heading>
+        <VStack space="xs">
+          <Text className="text-typography-500 leading-1">Email</Text>
+          <Input>
+            <InputField value={email} onChangeText={setEmail} type="text" />
+          </Input>
+        </VStack>
+        <VStack space="xs">
+          <Text className="text-typography-500 leading-1">Password</Text>
+          <Input className="text-center">
+            <InputField
+              value={password}
+              onChangeText={setPassword}
+              type={showPassword ? 'text' : 'password'}
+            />
+            <InputSlot className="pr-3">
+              <InputIcon
+                as={showPassword ? EyeIcon : EyeOffIcon}
+                className="text-darkBlue-500"
+              />
+            </InputSlot>
+          </Input>
+        </VStack>
+        <HStack space="sm">
+
+          <Button className="flex-1">
+            <ButtonText>Sign in</ButtonText>
+          </Button>
+        </HStack>
+      </VStack>
+    </FormControl>
+  );
+}
